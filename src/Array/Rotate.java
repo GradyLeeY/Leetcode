2c0cab6483ecbdc227642d8956c160e3d026e930
ly <1016627132@qq.com>
2020-05-25 17:47:27 +0800

    将图像顺时针旋转 90 度

---
+++ src/Array/Rotate.java
@@ -0,0 +1,40 @@
+package Array;
+
+/**
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ *
+ * 将图像顺时针旋转 90 度。
+ * @author grady
+ * @date 20-5-25 下午4:51
+ */
+public class Rotate {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        solution(matrix);
+    }
+    private static void solution(int[][] matrix){
+        int n = matrix.length;
+        //先转置
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+
+}
