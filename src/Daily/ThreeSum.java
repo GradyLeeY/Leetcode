deac3676d904697df9e98bc6814ca3be91262018
liying <ying.li@chinacreator.com>
2021-03-05 10:25:35 +0800

    三数之和：给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。

--- src/Daily/ThreeSum.java
+++ src/Daily/ThreeSum.java
@@ -1,5 +1,7 @@
 package Daily;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,9 +11,41 @@ import java.util.List;
 public class ThreeSum {
 
     public static void main(String[] args) {
-
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(solution(nums));
     }
-    private static List<List<Integer>> solution(int[] sums){
-        return null;
+
+    private static List<List<Integer>> solution(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int len = nums.length;
+        if (len < 3 || nums == null) {
+            return ans;
+        }
+        //排序
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0){
+                break;
+            }
+            //去重
+            if (i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L < R && nums[L] == nums[L+1]) L++;
+                    while (L < R && nums[R] == nums[R-1]) R--;
+                    L ++;
+                    R --;
+                }
+                else if (sum < 0) L++;
+                else if (sum > 0) R--;
+            }
+        }
+        return ans;
     }
 }
